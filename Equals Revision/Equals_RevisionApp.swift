01d2b195b6f40175a278e637f9fa84996120cfbc
Marc Boanas <marcjohnboanas@gmail.com>
2020-08-10 14:17:00 +0100

    first commit

---
+++ Equals Revision/Equals_RevisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Equals_RevisionApp.swift
+//  Equals Revision
+//
+//  Created by Marc Boanas on 10/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct Equals_RevisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
